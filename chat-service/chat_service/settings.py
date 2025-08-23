780cce4396764fb2e582095848df8b76eea43a3a
bmuia <belammuia0@gmail.com>
2025-08-23 11:14:49 +0300

    fix folder structure

--- chat-service/chat_service/settings.py
+++ chat-service/chat_service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5d#0gcy%a^2un!oox*+@3tbdb4!lo#q+kjs=xuc2!w5hl#!i6b'
+SECRET_KEY = 'django-insecure-jl@(5=2$2qyus%pxmv_s!)&3+2(f9c!5v^&*!(glratwmv!#63'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
